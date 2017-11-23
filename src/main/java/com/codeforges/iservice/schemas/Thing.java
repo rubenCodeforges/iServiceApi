46d56314b5dbe3e3fade6d5b66d3fa114fe54e95
codeforges <ruben@codeforges.com>
2017-11-23 21:04:00 +0100

    NGSH Schema proto

--- src/main/java/com/codeforges/iservice/schemas/Thing.java
+++ src/main/java/com/codeforges/iservice/schemas/Thing.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 public class Thing {
     private String identifier;
     private String name;
-    private String alternateName;
+    private String disambiguatingDescription;
     private String description;
     private String additionalDescription;
     private Collection<Image> images;
